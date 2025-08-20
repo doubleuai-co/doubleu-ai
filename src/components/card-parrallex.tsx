8ada3903e52396a6661e4a684d14b336069fb3de
Emmanuel <ovansaemmanuel@gmail.com>
2025-08-20 11:12:53 +0100

    final

--- src/components/card-parrallex.tsx
+++ src/components/card-parrallex.tsx
@@ -1,153 +1,153 @@
-"use client";
-import React, { useCallback, useEffect, useRef } from "react";
-import {
-  EmblaCarouselType,
-  EmblaEventType,
-  EmblaOptionsType,
-} from "embla-carousel";
-import useEmblaCarousel from "embla-carousel-react";
-import {
-  NextButton,
-  PrevButton,
-  usePrevNextButtons,
-} from "./EmbalaCarouselArrowButtons";
-import { DotButton, useDotButton } from "./EmbalaCarouselDotButton";
-import Image from "next/image";
+// "use client";
+// import React, { useCallback, useEffect, useRef } from "react";
+// import {
+//   EmblaCarouselType,
+//   EmblaEventType,
+//   EmblaOptionsType,
+// } from "embla-carousel";
+// import useEmblaCarousel from "embla-carousel-react";
+// import {
+//   NextButton,
+//   PrevButton,
+//   usePrevNextButtons,
+// } from "./EmbalaCarouselArrowButtons";
+// import { DotButton, useDotButton } from "./EmbalaCarouselDotButton";
+// import Image from "next/image";
 
-const TWEEN_FACTOR_BASE = 0.2;
+// const TWEEN_FACTOR_BASE = 0.2;
 
-type PropType = {
-  slides: number[];
-  options?: EmblaOptionsType;
-};
+// type PropType = {
+//   slides: number[];
+//   options?: EmblaOptionsType;
+// };
 
-const EmblaCarousel: React.FC<PropType> = (props) => {
-  const { slides, options } = props;
-  const [emblaRef, emblaApi] = useEmblaCarousel(options);
-  const tweenFactor = useRef(0);
-  const tweenNodes = useRef<HTMLElement[]>([]);
+// const EmblaCarousel: React.FC<PropType> = (props) => {
+//   const { slides, options } = props;
+//   const [emblaRef, emblaApi] = useEmblaCarousel(options);
+//   const tweenFactor = useRef(0);
+//   const tweenNodes = useRef<HTMLElement[]>([]);
 
-  const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi);
+//   const { selectedIndex, scrollSnaps, onDotButtonClick } =
+//     useDotButton(emblaApi);
 
-  const {
-    prevBtnDisabled,
-    nextBtnDisabled,
-    onPrevButtonClick,
-    onNextButtonClick,
-  } = usePrevNextButtons(emblaApi);
+//   const {
+//     prevBtnDisabled,
+//     nextBtnDisabled,
+//     onPrevButtonClick,
+//     onNextButtonClick,
+//   } = usePrevNextButtons(emblaApi);
 
-  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
-    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
-      return slideNode.querySelector("[data-parallax-layer]") as HTMLElement;
-    });
-  }, []);
+//   const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
+//     tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
+//       return slideNode.querySelector("[data-parallax-layer]") as HTMLElement;
+//     });
+//   }, []);
 
-  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
-    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
-  }, []);
+//   const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+//     tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+//   }, []);
 
-  const tweenParallax = useCallback(
-    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
-      const engine = emblaApi.internalEngine();
-      const scrollProgress = emblaApi.scrollProgress();
-      const slidesInView = emblaApi.slidesInView();
-      const isScrollEvent = eventName === "scroll";
+//   const tweenParallax = useCallback(
+//     (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+//       const engine = emblaApi.internalEngine();
+//       const scrollProgress = emblaApi.scrollProgress();
+//       const slidesInView = emblaApi.slidesInView();
+//       const isScrollEvent = eventName === "scroll";
 
-      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
-        let diffToTarget = scrollSnap - scrollProgress;
-        const slidesInSnap = engine.slideRegistry[snapIndex];
+//       emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+//         let diffToTarget = scrollSnap - scrollProgress;
+//         const slidesInSnap = engine.slideRegistry[snapIndex];
 
-        slidesInSnap.forEach((slideIndex) => {
-          if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+//         slidesInSnap.forEach((slideIndex) => {
+//           if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
 
-          if (engine.options.loop) {
-            engine.slideLooper.loopPoints.forEach((loopItem) => {
-              const target = loopItem.target();
+//           if (engine.options.loop) {
+//             engine.slideLooper.loopPoints.forEach((loopItem) => {
+//               const target = loopItem.target();
 
-              if (slideIndex === loopItem.index && target !== 0) {
-                const sign = Math.sign(target);
+//               if (slideIndex === loopItem.index && target !== 0) {
+//                 const sign = Math.sign(target);
 
-                if (sign === -1) {
-                  diffToTarget = scrollSnap - (1 + scrollProgress);
-                }
-                if (sign === 1) {
-                  diffToTarget = scrollSnap + (1 - scrollProgress);
-                }
-              }
-            });
-          }
+//                 if (sign === -1) {
+//                   diffToTarget = scrollSnap - (1 + scrollProgress);
+//                 }
+//                 if (sign === 1) {
+//                   diffToTarget = scrollSnap + (1 - scrollProgress);
+//                 }
+//               }
+//             });
+//           }
 
-          const translate = diffToTarget * (-1 * tweenFactor.current) * 100;
-          const tweenNode = tweenNodes.current[slideIndex];
-          tweenNode.style.transform = `translateX(${translate}%)`;
-        });
-      });
-    },
-    [],
-  );
+//           const translate = diffToTarget * (-1 * tweenFactor.current) * 100;
+//           const tweenNode = tweenNodes.current[slideIndex];
+//           tweenNode.style.transform = `translateX(${translate}%)`;
+//         });
+//       });
+//     },
+//     [],
+//   );
 
-  useEffect(() => {
-    if (!emblaApi) return;
+//   useEffect(() => {
+//     if (!emblaApi) return;
 
-    setTweenNodes(emblaApi);
-    setTweenFactor(emblaApi);
-    tweenParallax(emblaApi);
+//     setTweenNodes(emblaApi);
+//     setTweenFactor(emblaApi);
+//     tweenParallax(emblaApi);
 
-    emblaApi
-      .on("reInit", setTweenNodes)
-      .on("reInit", setTweenFactor)
-      .on("reInit", tweenParallax)
-      .on("scroll", tweenParallax)
-      .on("slideFocus", tweenParallax);
-  }, [emblaApi, tweenParallax]);
+//     emblaApi
+//       .on("reInit", setTweenNodes)
+//       .on("reInit", setTweenFactor)
+//       .on("reInit", tweenParallax)
+//       .on("scroll", tweenParallax)
+//       .on("slideFocus", tweenParallax);
+//   }, [emblaApi, tweenParallax]);
 
-  return (
-    <div className="mx-auto max-w-[38rem]">
-      <div className="overflow-hidden" ref={emblaRef}>
-        <div className="-ml-1 flex touch-pan-y touch-pinch-zoom md:-ml-4">
-          {slides.map((index) => (
-            <div
-              className="min-w-0 flex-[0_0_60%] transform pl-1 md:flex-[0_0_80%] md:pl-4"
-              key={index}
-            >
-              <div className="h-full overflow-hidden rounded-[0.43rem]">
-                <div
-                  className="relative flex h-full w-full justify-center"
-                  data-parallax-layer
-                >
-                  <Image
-                    className="block h-[10.68rem] w-[9.75rem] max-w-none flex-[0_0_calc(115%_+_(0.5rem_*_2))] rounded-[0.43rem] object-contain md:h-[28rem] md:w-[25rem] md:flex-[0_0_calc(115%_+_(1rem_*_2))]"
-                    src={`/card${index + 1}.png`}
-                    alt="Your alt text"
-                    width={402}
-                    height={443}
-                  />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+//   return (
+//     <div className="mx-auto max-w-[38rem]">
+//       <div className="overflow-hidden" ref={emblaRef}>
+//         <div className="-ml-1 flex touch-pan-y touch-pinch-zoom md:-ml-4">
+//           {slides.map((index) => (
+//             <div
+//               className="min-w-0 flex-[0_0_60%] transform pl-1 md:flex-[0_0_80%] md:pl-4"
+//               key={index}
+//             >
+//               <div className="h-full overflow-hidden rounded-[0.43rem]">
+//                 <div
+//                   className="relative flex h-full w-full justify-center"
+//                   data-parallax-layer
+//                 >
+//                   <Image
+//                     className="block h-[10.68rem] w-[9.75rem] max-w-none flex-[0_0_calc(115%_+_(0.5rem_*_2))] rounded-[0.43rem] object-contain md:h-[28rem] md:w-[25rem] md:flex-[0_0_calc(115%_+_(1rem_*_2))]"
+//                     src={`/card${index + 1}.png`}
+//                     alt="Your alt text"
+//                     width={402}
+//                     height={443}
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
 
-      <div className="mt-[1.8rem] grid grid-cols-[auto_1fr] justify-between gap-[1.2rem]">
-        <div className="grid grid-cols-2 items-center gap-[0.6rem]">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
+//       <div className="mt-[1.8rem] grid grid-cols-[auto_1fr] justify-between gap-[1.2rem]">
+//         <div className="grid grid-cols-2 items-center gap-[0.6rem]">
+//           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+//           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+//         </div>
 
-        <div className="mr-[0.6rem] flex flex-wrap items-center justify-end">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              isSelected={index === selectedIndex}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+//         <div className="mr-[0.6rem] flex flex-wrap items-center justify-end">
+//           {scrollSnaps.map((_, index) => (
+//             <DotButton
+//               key={index}
+//               onClick={() => onDotButtonClick(index)}
+//               isSelected={index === selectedIndex}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default EmblaCarousel;
+// export default EmblaCarousel;
